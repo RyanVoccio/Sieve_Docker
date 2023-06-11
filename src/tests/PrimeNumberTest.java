7c635f1ea0eed376397abac42c2eee3bbd091259
RyanVoccio <ryanpvoccio@gmail.com>
2023-06-11 16:40:45 -0500

    adding source

---
+++ src/tests/PrimeNumberTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import voccio.assignment4.PrimeCalculator;
+
+public class PrimeNumberTest {
+
+	@Test
+	public void test() {
+
+        System.out.println("This is the testcase in this class");
+        
+        PrimeCalculator calculator = new PrimeCalculator();
+		
+		List<Integer> primes = calculator.GetPrimeNumbers(10);
+		List<Integer> expectedList = new ArrayList<Integer>();
+		expectedList.add(2);
+		expectedList.add(3);
+		expectedList.add(5);
+		expectedList.add(7);
+		assertEquals(true, true);
+	}
+
+}
